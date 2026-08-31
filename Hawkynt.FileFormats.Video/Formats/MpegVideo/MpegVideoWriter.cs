40d3300b970f2bba559480f6816aedc41654db32
Hawkynt <github@synthelicz.de>
2026-08-31 08:38:06 +0200

    + preserve MPEG elementary trailers on write

--- Hawkynt.FileFormats.Video/Formats/MpegVideo/MpegVideoWriter.cs
+++ Hawkynt.FileFormats.Video/Formats/MpegVideo/MpegVideoWriter.cs
@@ -1,4 +1,6 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using FileFormat.Core;
 using Hawkynt.FileFormats.Video;
 
@@ -11,7 +13,9 @@ public sealed class MpegVideoWriter : IVideoContainerWriter<MpegVideoWriter> {
 
   private MpegVideoWriter(IReadOnlyList<MediaStreamInfo> streams, VideoMetadata metadata)
     => this._muxer = new(streams, metadata, "MPEG elementary video stream",
-      static stream => stream.Codec == CodecTag.FromCharacters("MPG1") || stream.Codec == CodecTag.FromCharacters("MPG2"));
+      static stream => stream.CodecPrivateData.IsEmpty
+        && (stream.Codec.EqualsIgnoringCase(CodecTag.FromCharacters("MPG1"))
+          || stream.Codec.EqualsIgnoringCase(CodecTag.FromCharacters("MPG2"))));
 
   public static string PrimaryExtension => ".m1v";
 
@@ -21,7 +25,23 @@ public sealed class MpegVideoWriter : IVideoContainerWriter<MpegVideoWriter> {
   public static MpegVideoWriter Create(IReadOnlyList<MediaStreamInfo> streams, VideoMetadata metadata)
     => new(streams, metadata);
 
-  public void WritePacket(CodedPacket packet) => this._muxer.WritePacket(packet);
+  public void WritePacket(CodedPacket packet) {
+    var trailer = packet.ContainerPrivateData.Span;
+    if (trailer.IsEmpty) {
+      this._muxer.WritePacket(packet);
+      return;
+    }
+
+    if (trailer.Length < 4
+        || trailer[^4] != 0x00 || trailer[^3] != 0x00 || trailer[^2] != 0x01 || trailer[^1] != MpegVideoReader.SequenceEndCode)
+      throw new InvalidDataException(
+        "MPEG elementary packet private data must end in the sequence-end start code 00 00 01 B7.");
+
+    var bytes = new byte[checked(packet.Data.Length + trailer.Length)];
+    packet.Data.Span.CopyTo(bytes);
+    trailer.CopyTo(bytes.AsSpan(packet.Data.Length));
+    this._muxer.WritePacket(packet with { Data = bytes, ContainerPrivateData = default });
+  }
 
   public byte[] Finish() => this._muxer.Finish();
 }
