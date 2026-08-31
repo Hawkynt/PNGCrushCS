ae5574427c7bcac727c923615887ed3b9b113eb0
Hawkynt <github@synthelicz.de>
2026-08-31 16:52:01 +0200

    diagnose remaining H.264 High-profile frame hashes

---
+++ Tests/Hawkynt.FileFormats.Video.Tests/Codecs/H264/H264CabacHighProfileHashDiagnosticTests.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Security.Cryptography;
+using FileFormat.Core;
+using FileFormat.H264Video;
+
+namespace FileFormat.Codecs.H264.Tests;
+
+[TestFixture]
+public sealed class H264CabacHighProfileHashDiagnosticTests {
+  [Test]
+  public void ReportAllHighProfileIpbbHashes() {
+    var field = typeof(H264CabacHighProfileConformanceTests).GetField(
+      "_CABAC_HIGH_8X8_IPBB",
+      BindingFlags.NonPublic | BindingFlags.Static);
+    Assert.That(field, Is.Not.Null);
+    var encoded = Convert.FromBase64String((string)field!.GetRawConstantValue()!);
+    var stream = new MediaStreamInfo {
+      Index = 0,
+      Kind = MediaStreamKind.Video,
+      Codec = CodecTag.FromCharacters("avc1"),
+    };
+    var decoder = H264VideoDecoder.Create(stream);
+    var frames = new List<RawImage>();
+    foreach (var packet in H264VideoReader.Split(encoded))
+      if (decoder.TryDecode(packet, out var frame))
+        frames.Add(frame);
+    frames.AddRange(decoder.Flush());
+
+    Assert.That(frames, Has.Count.EqualTo(4));
+    var hashes = new string[frames.Count];
+    for (var index = 0; index < frames.Count; ++index)
+      hashes[index] = Convert.ToHexString(SHA256.HashData(frames[index].PixelData));
+
+    Assert.Fail($"High-profile IPBB hashes: {string.Join(", ", hashes)}");
+  }
+}
