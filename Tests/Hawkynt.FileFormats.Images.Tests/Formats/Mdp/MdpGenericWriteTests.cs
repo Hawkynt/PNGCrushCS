0f12c48c64d4b3d6564601bfd24ace685952226d
Hawkynt <github@synthelicz.de>
2026-09-03 09:45:08 +0200

    Test generic MDP write coverage

---
+++ Tests/Hawkynt.FileFormats.Images.Tests/Formats/Mdp/MdpGenericWriteTests.cs
@@ -0,0 +1,35 @@
+using FileFormat.Core;
+
+namespace FileFormat.Mdp.Tests;
+
+[TestFixture]
+public sealed class MdpGenericWriteTests {
+
+  [Test]
+  [Category("Integration")]
+  public void FromRawImage_ThroughInterface_UsesCanonicalPageMetadataAndRoundTrips() {
+    var pixels = new byte[8 * 4 * 3];
+    Array.Fill(pixels, (byte)255);
+    var source = new RawImage {
+      Width = 8,
+      Height = 4,
+      Format = PixelFormat.Rgb24,
+      PixelData = pixels,
+    };
+
+    var file = _Encode<MdpFile>(source);
+    var restored = MdpReader.FromBytes(MdpWriter.ToBytes(file));
+
+    Assert.Multiple(() => {
+      Assert.That(file.Resolution, Is.EqualTo(MdpFile.DefaultResolution));
+      Assert.That(file.PageFormat, Is.EqualTo(MdpFile.DefaultPageFormat));
+      Assert.That(file.PageRamBlocks, Is.EqualTo(1));
+      Assert.That(file.SerialNumber, Is.EqualTo("0000000"));
+      Assert.That(file.RasterData, Is.EqualTo(new byte[] { 0xFF, 0xFF, 0xFF, 0xFF }));
+      Assert.That(restored, Is.EqualTo(file));
+    });
+  }
+
+  private static TFile _Encode<TFile>(RawImage image) where TFile : IImageFromRawImage<TFile>
+    => TFile.FromRawImage(image);
+}
