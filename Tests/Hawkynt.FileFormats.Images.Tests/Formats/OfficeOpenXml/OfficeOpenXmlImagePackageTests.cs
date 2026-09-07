21ad818c80e907f4a02a59bd271d24d1f92dbeeb
Hawkynt <github@synthelicz.de>
2026-09-07 16:07:01 +0200

    Fix Office OOXML test imports

--- Tests/Hawkynt.FileFormats.Images.Tests/Formats/OfficeOpenXml/OfficeOpenXmlImagePackageTests.cs
+++ Tests/Hawkynt.FileFormats.Images.Tests/Formats/OfficeOpenXml/OfficeOpenXmlImagePackageTests.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Text;
 using FileFormat.Core;
 using FileFormat.Excel;
