afd51136c30d3dfd7a49bb599031a87037abc56a
Hawkynt <github@synthelicz.de>
2026-09-15 07:51:23 +0200

    Fix unsupported Word extension test

--- Tests/Hawkynt.FileFormats.Images.Tests/Formats/OfficeOpenXml/OfficeOpenXmlImagePackageTests.cs
+++ Tests/Hawkynt.FileFormats.Images.Tests/Formats/OfficeOpenXml/OfficeOpenXmlImagePackageTests.cs
@@ -129,7 +129,7 @@ public sealed class OfficeOpenXmlImagePackageTests {
     var source = _Picture(2, 2);
 
     Assert.Multiple(() => {
-      Assert.Throws<ArgumentException>(() => WordFile.FromRawImage(source, ".doc"));
+      Assert.Throws<ArgumentException>(() => WordFile.FromRawImage(source, ".odt"));
       Assert.Throws<ArgumentException>(() => ExcelFile.FromRawImage(source, ".ods"));
       Assert.Throws<ArgumentException>(() => PowerPointFile.FromRawImage(source, ".odp"));
     });
