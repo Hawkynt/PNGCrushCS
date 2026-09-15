3d2ca28b5f1ee9480fb4e9c40e70dd396f6f76b7
Hawkynt <github@synthelicz.de>
2026-09-15 09:28:15 +0200

    + Guard CMV frame allocation arithmetic

--- Hawkynt.FileFormats.Video/Codecs/Ea/EaCmvFrame.cs
+++ Hawkynt.FileFormats.Video/Codecs/Ea/EaCmvFrame.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace FileFormat.Codecs.Ea;
 
 /// <summary>One reconstructed Electronic Arts CMV picture: eight-bit palette indices, one byte a pixel.</summary>
@@ -6,7 +8,7 @@ internal sealed class EaCmvFrame {
   internal EaCmvFrame(int width, int height) {
     this.Width = width;
     this.Height = height;
-    this.Indices = new byte[width * height];
+    this.Indices = new byte[checked(width * height)];
   }
 
   internal int Width { get; }
