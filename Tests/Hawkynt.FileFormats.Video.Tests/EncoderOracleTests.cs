4b90739e1bae6e5cc92318f0247dbd386612d308
Hawkynt <github@synthelicz.de>
2026-09-15 09:34:59 +0200

    Let the encoder oracle exercise RPL

--- Tests/Hawkynt.FileFormats.Video.Tests/EncoderOracleTests.cs
+++ Tests/Hawkynt.FileFormats.Video.Tests/EncoderOracleTests.cs
@@ -13,6 +13,7 @@ using FileFormat.Mjpeg;
 using FileFormat.Ogg;
 using FileFormat.RealMedia;
 using FileFormat.Mp4;
+using FileFormat.Rpl;
 using FileFormat.RoqVideo;
 using FileFormat.Vqa;
 using FileFormat.Yuv4Mpeg;
@@ -264,6 +265,7 @@ public sealed class EncoderOracleTests {
     ("Roq", ".roq", static (streams, packets) => VideoIO.Mux<RoqWriter>(streams, packets)),
     ("Ea", ".cmv", static (streams, packets) => VideoIO.Mux<EaWriter>(streams, packets)),
     ("Vqa", ".vqa", static (streams, packets) => VideoIO.Mux<VqaWriter>(streams, packets)),
+    ("Rpl", ".rpl", static (streams, packets) => VideoIO.Mux<RplWriter>(streams, packets)),
     ("Mjpeg", ".mjpg", static (streams, packets) => VideoIO.Mux<MjpegWriter>(streams, packets)),
     ("Ogg", ".ogv", static (streams, packets) => VideoIO.Mux<OggWriter>(streams, packets)),
   ];
